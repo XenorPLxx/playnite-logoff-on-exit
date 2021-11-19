64a81eb7484f27e0c7109f21e115d0c6cfa05b31
Dawid Karczewski <dawidxen@gmail.com>
2021-11-19 21:56:54 +0100

    Creates script

---
+++ LogoffOnExit.psm1
@@ -0,0 +1,51 @@
+function OnApplicationStarted()
+{
+    $__logger.Info("OnApplicationStarted")
+}
+
+function OnApplicationStopped()
+{
+    (Get-WmiObject -Class Win32_OperatingSystem).Win32Shutdown(0) # 0 Is a Flag
+    $__logger.Info("OnApplicationStopped")
+}
+
+function OnLibraryUpdated()
+{
+    $__logger.Info("OnLibraryUpdated")
+}
+
+function OnGameStarting()
+{
+    param($evnArgs)
+    $__logger.Info("OnGameStarting $($evnArgs.Game)")
+}
+
+function OnGameStarted()
+{
+    param($evnArgs)
+    $__logger.Info("OnGameStarted $($evnArgs.Game)")
+}
+
+function OnGameStopped()
+{
+    param($evnArgs)
+    $__logger.Info("OnGameStopped $($evnArgs.Game) $($evnArgs.ElapsedSeconds)")
+}
+
+function OnGameInstalled()
+{
+    param($evnArgs)
+    $__logger.Info("OnGameInstalled $($evnArgs.Game)")
+}
+
+function OnGameUninstalled()
+{
+    param($evnArgs)
+    $__logger.Info("OnGameUninstalled $($evnArgs.Game)")
+}
+
+function OnGameSelected()
+{
+    param($gameSelectionEventArgs)
+    $__logger.Info("OnGameSelected $($gameSelectionEventArgs.OldValue) -> $($gameSelectionEventArgs.NewValue)")
+}
